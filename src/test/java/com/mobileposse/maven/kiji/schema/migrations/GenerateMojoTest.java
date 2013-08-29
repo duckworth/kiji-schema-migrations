9b84ac3d28b72348142c0c5c6d8008d889478051
John Lemp <john@graavy.com>
2013-08-29 08:20:48 -0400

    generate goal

---
+++ src/test/java/com/mobileposse/maven/kiji/schema/migrations/GenerateMojoTest.java
@@ -0,0 +1,36 @@
+package com.mobileposse.maven.kiji.schema.migrations;
+
+import java.io.File;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+/**
+ * Resets the migration version
+ */
+public class GenerateMojoTest extends AbstractMojoTestCase {
+
+    /** {@inheritDoc} */
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /** {@inheritDoc} */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * @throws Exception if any
+     */
+    public void testResetDefault() throws Exception {
+        File testBasedir = getTestFile("src/test/resources/unit/project-to-test");
+        File pom = new File(testBasedir, "pom.xml");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        ResetMojo resetMojo = (ResetMojo) lookupMojo("reset", pom);
+        assertNotNull(resetMojo);
+        resetMojo.kijiURI = "kiji://.env/default";
+        resetMojo.execute();
+    }
+}
